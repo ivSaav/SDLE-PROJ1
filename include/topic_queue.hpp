b71d282aa600e760d35ae1b447a6fdb16011f150
João Martins <joaolucassilvamartins@gmail.com>
2021-11-16 12:45:42 +0000

    Fix: Sigsev when queue resized. Have one bug that is still WIP

--- include/topic_queue.hpp
+++ include/topic_queue.hpp
@@ -1,28 +1,30 @@
 #pragma once
+#include <list>
 #include <map>
-#include <queue>
 #include <string>
 #include <unordered_map>
 #include <utility>
 
 using namespace std;
 
+typedef map<string, list<string>::iterator> peer_iterator_map;
+
 /*
    This class is responsible for handling the message queue
    and managing each peer's position in it
 */
 class TopicQueue {
-
 private:
   // map<topic_name, map<peer_id, iterator>>
-  unordered_map<string, map<string, deque<string>::iterator>> peer_positions;
+  unordered_map<string, peer_iterator_map> peer_positions;
   // map<topic_name, queue>
-  unordered_map<string, deque<string>> queues;
+  unordered_map<string, list<string>> queues;
 
-  deque<string> &getQueue(const string topic_name);
+  list<string> &getQueue(const string topic_name);
 
   bool getIterator(string topic_name, string peer_id,
-                   deque<string>::iterator &iter);
+                   list<string>::iterator &iter);
+  bool contains_subscribed(string topic_name) const;
 
 public:
   TopicQueue() {}
--- src/topic_queue.cpp
+++ src/topic_queue.cpp
@@ -3,28 +3,35 @@
 #include <iostream>
 
 /* PRIVATE */
-deque<string> &TopicQueue::getQueue(const string topic_name) {
+list<string> &TopicQueue::getQueue(const string topic_name) {
   if (this->queues.find(topic_name) == this->queues.end()) {
     // Need to create new queue
-    deque<string> q;
+    list<string> q;
     this->queues.insert({topic_name, q});
   }
 
   return this->queues.at(topic_name);
 }
 
+bool TopicQueue::contains_subscribed(string topic_name) const {
+  if (this->peer_positions.find(topic_name) == this->peer_positions.end())
+    return false; // No peer subbed on this topic
+
+  const peer_iterator_map &id_to_queue = this->peer_positions.at(topic_name);
+  return id_to_queue.size() > 0;
+}
+
 void TopicQueue::trimQueue(string topic_name) {
-  deque<string> &q = this->getQueue(topic_name);
+  list<string> &q = this->getQueue(topic_name);
   if (q.empty())
     return;
 
   if (this->peer_positions.find(topic_name) == this->peer_positions.end())
     return; // No iterator
 
-  map<string, deque<string>::iterator> &id_to_queue =
-      this->peer_positions.at(topic_name);
+  peer_iterator_map &id_to_queue = this->peer_positions.at(topic_name);
 
-  deque<string>::iterator iter = id_to_queue.begin()->second;
+  list<string>::iterator iter = id_to_queue.begin()->second;
 
   auto lowest_iter = std::min_element(id_to_queue.begin(), id_to_queue.end(),
                                       [&q](const auto &a, const auto &b) {
@@ -48,19 +55,18 @@ bool TopicQueue::is_subscribed(string peer_id, string topic_name) const {
 void TopicQueue::subscribe(string peer_id, string topic_name) {
   // If no map, create it
   if (this->peer_positions.find(topic_name) == this->peer_positions.end())
-    this->peer_positions.insert(
-        {topic_name, map<string, deque<string>::iterator>()});
+    this->peer_positions.insert({topic_name, peer_iterator_map()});
 
   // Get map that points to queues
-  map<string, deque<string>::iterator> &id_to_queue =
-      this->peer_positions.at(topic_name);
+  peer_iterator_map &id_to_queue = this->peer_positions.at(topic_name);
 
   // Add reference of peer_id to the map
-  deque<string> &queue = this->getQueue(topic_name);
+  list<string> &queue = this->getQueue(topic_name);
   if (id_to_queue.find(peer_id) == id_to_queue.end()) {
     id_to_queue.insert({peer_id, queue.begin()});
   }
 
+  cout << *this << endl;
   // Peer was already subscribed
   return;
 }
@@ -70,8 +76,7 @@ void TopicQueue::unsubscribe(string peer_id, string topic_name) {
   if (this->peer_positions.find(topic_name) == this->peer_positions.end())
     return; // Nothing to remove
   else {
-    map<string, deque<string>::iterator> &id_to_queue =
-        this->peer_positions.at(topic_name);
+    peer_iterator_map &id_to_queue = this->peer_positions.at(topic_name);
 
     // Get reference of peer in map
     if (id_to_queue.find(peer_id) == id_to_queue.end()) {
@@ -86,8 +91,19 @@ void TopicQueue::unsubscribe(string peer_id, string topic_name) {
 }
 
 void TopicQueue::put(string topic_name, string content) {
-  deque<string> &queue = this->getQueue(topic_name);
+  if (!this->contains_subscribed(topic_name))
+    return; // No need to append if nobody is subbed
+
+  list<string> &queue = this->getQueue(topic_name);
+
   queue.push_back(content);
+  if (queue.size() == 1) { // We just appended the first element
+    peer_iterator_map &iter_map = this->peer_positions.at(topic_name);
+    // For each peer that consumed all, update to new pos
+    for (auto &peerid_iter : iter_map) {
+      peerid_iter.second = queue.begin();
+    }
+  }
 
   return;
 }
@@ -96,13 +112,17 @@ bool TopicQueue::get(string peer_id, string topic_name, string &content) {
   if (!is_subscribed(peer_id, topic_name))
     return false;
 
-  const deque<string> &queue = this->getQueue(topic_name);
-  deque<string>::iterator &iter =
+  const list<string> &queue = this->getQueue(topic_name);
+  list<string>::iterator &iter =
       this->peer_positions.at(topic_name).at(peer_id);
   if (iter == queue.end()) // We have no more messages
     return false;
 
+  cout << "BEFORE";
+  fflush(stdout);
   content = *iter;
+  cout << "AFTER" << endl;
+  fflush(stdout);
   ++iter;
   this->trimQueue(topic_name);
   return true;
@@ -112,7 +132,7 @@ ostream &operator<<(ostream &os, TopicQueue &q) {
   string sep = "---------------------------------------";
   os << "Queues:" << endl;
   for (auto i = q.queues.begin(); i != q.queues.end(); ++i) {
-    deque<string> curr = (i->second);
+    list<string> &curr = (i->second);
     os << i->first + ": ";
     for (auto j = curr.begin(); j != curr.end(); ++j) {
       os << (*j) + " ";
@@ -122,16 +142,16 @@ ostream &operator<<(ostream &os, TopicQueue &q) {
   os << sep << endl;
   os << "Maps:" << endl;
   for (auto i = q.peer_positions.begin(); i != q.peer_positions.end(); ++i) {
-    map<string, deque<string>::iterator> curr = i->second;
+    peer_iterator_map &curr = i->second;
     const string topic_name = i->first;
-    auto queue = q.getQueue(topic_name);
+    list<string> &queue = q.getQueue(topic_name);
 
     os << topic_name + ": ";
-    for (auto j = curr.begin(); j != curr.end(); ++j) {
-      if (j->second != queue.end())
-        os << j->first + "-" + *(j->second) + " ";
+    for (auto &j : curr) {
+      if (queue.size() != 0 && j.second != queue.end())
+        os << j.first + "-" + *(j.second) + " ";
       else
-        os << j->first + "-END" + " ";
+        os << j.first + "-END" + " ";
     }
     os << endl;
   }
