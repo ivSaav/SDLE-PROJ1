92f838516c60efe63a64153a9c93e35da662b16e
Joao Lucas <joaolucassilvamartins@gmail.com>
2021-11-25 10:36:50 +0000

    Fix: Some cout formatting

--- src/node.cpp
+++ src/node.cpp
@@ -22,9 +22,7 @@ int Node::send_request(Message *m, string &request_id, int max_retries) {
   while (n_tries < max_retries && !done) {
     ++n_tries;
 
-  #ifdef DEBUG
     cout << "SENDING: " << req_msg.to_string() << endl;
-  #endif
     zmqpp::message msg = req_msg.to_zmq_msg();
     this->socket.send(msg);
 
@@ -46,10 +44,11 @@ int Node::send_request(Message *m, string &request_id, int max_retries) {
   return done ? 0 : -1;
 }
 
-// Returns -1 if request wasn't made to the server. 
+// Returns -1 if request wasn't made to the server.
 // 1 If the request is still being processed by the server
 // 0 If success
-int Node::get_request(string request_id, shared_ptr<Message> &response, int max_retries) {
+int Node::get_request(string request_id, shared_ptr<Message> &response,
+                      int max_retries) {
   int num_tries = 0;
   bool done = false;
   while (num_tries < max_retries && !done) {
@@ -58,9 +57,9 @@ int Node::get_request(string request_id, shared_ptr<Message> &response, int max_
 
     // Send get request
     TitanicMessage get_msg(TitanicMessage::GET_TIT, request_id);
-  #ifdef DEBUG
+#ifdef DEBUG
     cout << "SENDING: " << get_msg.to_string() << endl;
-  #endif
+#endif
     zmqpp::message get = get_msg.to_zmq_msg();
     this->socket.send(get);
 
@@ -74,7 +73,8 @@ int Node::get_request(string request_id, shared_ptr<Message> &response, int max_
       if (s == zmqpp::signal::ko) { // Still waiting for server
         cout << "Server doesn't have request yet, retrying" << endl;
         usleep(RESEND_TIMEOUT);
-      } else if (s == zmqpp::signal::stop) { // Server has no knowledge of request
+      } else if (s ==
+                 zmqpp::signal::stop) { // Server has no knowledge of request
         return -1;
       }
     } else { // Normal message
@@ -84,9 +84,9 @@ int Node::get_request(string request_id, shared_ptr<Message> &response, int max_
       if (type == msg_type::ANSWER)
         m = make_shared<AnswerMessage>(AnswerMessage(zmq_response));
       else
-        m = make_shared<Message>(Message(zmq_response, (msg_type) type));
+        m = make_shared<Message>(Message(zmq_response, (msg_type)type));
 
-      cout << "RECEIVED MESSAGE " << m->to_string() << endl;
+      cout << "\tRECEIVED MESSAGE " << m->to_string() << endl;
       response = m;
       done = true;
     }
@@ -95,7 +95,8 @@ int Node::get_request(string request_id, shared_ptr<Message> &response, int max_
   return done ? 0 : 1;
 }
 
-// Returns 0 if server deletes the request, -1 if the received msg doesn't make sense
+// Returns 0 if server deletes the request, -1 if the received msg doesn't make
+// sense
 int Node::delete_request(string request_id) {
   int num_tries = 0;
   // Send delete request
@@ -116,11 +117,7 @@ int Node::delete_request(string request_id) {
   return -1;
 }
 
-void Node::make_request(Message *request, Message &response) {
-  string req_id;
-
-}
-
+void Node::make_request(Message *request, Message &response) { string req_id; }
 
 /* PUBLIC */
 
@@ -138,15 +135,17 @@ int Node::subscribe(std::string topic_name) {
   shared_ptr<Message> response;
   string req_id;
 
-  if (this->send_request(&sub_msg, req_id)) throw FailedRequestBegin(topic_name);
+  if (this->send_request(&sub_msg, req_id))
+    throw FailedRequestBegin(topic_name);
   if (this->get_request(req_id, response)) {
     this->delete_request(req_id);
     throw FailedRequestRetreive(topic_name);
   }
-  if (this->delete_request(req_id)) throw FailedRequestDelete(topic_name);
+  if (this->delete_request(req_id))
+    throw FailedRequestDelete(topic_name);
 
   if (response->get_type() == msg_type::KO)
-   throw AlreadySubscribed(topic_name);
+    throw AlreadySubscribed(topic_name);
   if (response->get_type() == msg_type::OK)
     return 0;
 
@@ -158,16 +157,17 @@ int Node::unsubscribe(std::string topic_name) {
   shared_ptr<Message> response;
   string req_id;
 
-
-  if (this->send_request(&unsub_msg, req_id)) throw FailedRequestBegin(topic_name);
+  if (this->send_request(&unsub_msg, req_id))
+    throw FailedRequestBegin(topic_name);
   if (this->get_request(req_id, response)) {
     this->delete_request(req_id);
     throw FailedRequestRetreive(topic_name);
   }
-  if (this->delete_request(req_id)) throw FailedRequestDelete(topic_name);
+  if (this->delete_request(req_id))
+    throw FailedRequestDelete(topic_name);
 
   if (response->get_type() == msg_type::KO)
-   throw NotSubscribed(topic_name);
+    throw NotSubscribed(topic_name);
 
   if (response->get_type() == msg_type::OK)
     return 0;
@@ -182,7 +182,8 @@ int Node::get(string topic_name, string &content) {
 
   bool done = false;
   while (!done) { // Get must be blocking
-    if (this->send_request(&get_msg, req_id)) continue; // Can't connect to server
+    if (this->send_request(&get_msg, req_id))
+      continue; // Can't connect to server
 
     int get_response = this->get_request(req_id, response);
     if (get_response == -1) { // Can't establish connection with server
@@ -192,12 +193,13 @@ int Node::get(string topic_name, string &content) {
       continue;
     }
 
-    if (this->delete_request(req_id)) throw FailedRequestDelete(topic_name);
+    if (this->delete_request(req_id))
+      throw FailedRequestDelete(topic_name);
 
     if (response->get_type() == msg_type::KO)
-     throw NotSubscribed(topic_name);
+      throw NotSubscribed(topic_name);
 
-    AnswerMessage* answer_response = (AnswerMessage*) &(*response);
+    AnswerMessage *answer_response = (AnswerMessage *)&(*response);
     content = answer_response->get_body();
     if (content == "") {
       usleep(RESEND_TIMEOUT);
@@ -217,21 +219,21 @@ int Node::put(std::string topic_name, std::string content) {
   shared_ptr<Message> response;
   string req_id;
 
-  if (this->send_request(&put_msg, req_id)) throw FailedRequestBegin(topic_name);
-  if (this->get_request(req_id, response)) throw FailedRequestRetreive(topic_name);
-  if (this->delete_request(req_id)) throw FailedRequestDelete(topic_name);
+  if (this->send_request(&put_msg, req_id))
+    throw FailedRequestBegin(topic_name);
+  if (this->get_request(req_id, response))
+    throw FailedRequestRetreive(topic_name);
+  if (this->delete_request(req_id))
+    throw FailedRequestDelete(topic_name);
 
   if (response->get_type() == msg_type::OK)
-   return 0;
+    return 0;
   return 1;
 }
-
 
 void Node::print() {
   socket.send(zmqpp::signal::test);
   receive_ack(socket);
 }
 
-string Node::getId() const {
-  return id;
-}
+string Node::getId() const { return id; }
