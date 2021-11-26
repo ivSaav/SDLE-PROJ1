dbcf4eaa9ce72aba3149fe7a6507feb4eb5aa6de
Ana Barros <anaiobarros2014@gmail.com>
2021-11-26 16:28:46 +0000

    Add: test 3

--- config/test.sh
+++ config/test.sh
@@ -3,14 +3,18 @@
 testApp="../build/apps/test_app"
 broker="../build/apps/broker"
 
-read -p "Enter test number (1 or 2): " t
+read -p "Enter test number (1 to 3): " t
 
 if test $t -eq 1
 then
-	$broker & $testApp t1n1.txt 1 & $testApp t1n2.txt 2 & $testApp t1n3.txt 3 
+	$testApp t1n1.txt 1 & $testApp t1n2.txt 2 & $testApp t1n3.txt 3 
 else 
     if test $t -eq 2
     then
-	    $broker & $testApp t2n1.txt 1 & $testApp t2n2.txt 2
+	    $testApp t2n1.txt 1 & $testApp t2n2.txt 2
+    else 
+        if test $t -eq 3
+        then 
+            $testApp t3n1.txt 1 & $testApp t3n2.txt 2 & $testApp t3n2.txt 3 
     fi
 fi
